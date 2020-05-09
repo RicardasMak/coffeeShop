242c7c812576e510a78f38837d6f3ed76d24a0a7
flamingsanta <ricardas.makstutis@gmail.com>
2020-05-09 08:58:29 +0100

    update

--- src/AccountManager.php
+++ src/AccountManager.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Tudublin;
 
 
@@ -11,7 +12,6 @@ class AccountManager
     {
         $this->db = new Db();
         $this->mainController = new MainController($twig);
-
     }
 
     //delete account
@@ -19,25 +19,23 @@ class AccountManager
     {
         $userDelete = filter_input(INPUT_POST, 'userNameDelete');
 
-        if(!empty($userDelete))
-        {
+        if (!empty($userDelete)) {
             $acc = $this->db->findUser($userDelete);
 
             if ($acc != null) {
                 $this->db->deleteUser($userDelete);
 
                 $message = ['account deleted successfully'];
-                $this->mainController->secret($temp=null, $message);
-            }
-            else{
+                $this->mainController->secret($temp = null, $message);
+            } else {
 
                 $error = ['user not found'];
-                $this->mainController->secret($temp=null, $error);
+                $this->mainController->secret($temp = null, $error);
             }
-        }else{
+        } else {
 
             $error = ['field is empty'];
-            $this->mainController->secret($temp=null, $error);
+            $this->mainController->secret($temp = null, $error);
         }
     }
 
@@ -54,67 +52,62 @@ class AccountManager
         $userNameCheck = $this->checkUserName($userName);
         $passwordCheck = $this->passwordCheck($password, $passConfirm);
 
-        if(sizeof($userNameCheck) == 0 and sizeof($passwordCheck) == 0)
-        {
-          $passwordHashed = $this->hashPassword($password);
-          $this->db->createUser($userName, $passwordHashed, $role, $payed);
+        if (sizeof($userNameCheck) == 0 and sizeof($passwordCheck) == 0) {
+            $passwordHashed = $this->hashPassword($password);
+            $this->db->createUser($userName, $passwordHashed, $role, $payed);
 
-          $message = ['account crated'];
-          $this->mainController->secret($message);
-        }
-        elseif(sizeof($userNameCheck)>0){
+            $message = ['account crated'];
+            $this->mainController->secret($message);
+        } elseif (sizeof($userNameCheck) > 0) {
 
-          $this->mainController->secret($userNameCheck);
+            $this->mainController->secret($userNameCheck);
 
-        }elseif(sizeof($passwordCheck)>0)
-        {
-          $this->mainController->secret($passwordCheck);
-        }else{
+        } elseif (sizeof($passwordCheck) > 0) {
+            $this->mainController->secret($passwordCheck);
+        } else {
 
-          $this->mainController->secret();
+            $this->mainController->secret();
         }
     }
+
     //check password
     private function passwordCheck($password, $passwordConfirm)
     {
-        $error =[];
+        $error = [];
 
-        if(empty($password) and empty($passwordConfirm))
-        {
+        if (empty($password) and empty($passwordConfirm)) {
             $error = ['password field is empty'];
-        }
-        elseif($password != $passwordConfirm)
-        {
+        } elseif ($password != $passwordConfirm) {
             $error = ['password doesnt match'];
         }
 
         return $error;
-
     }
-    //check user name
-   private function checkUserName($userName)
-   {
-       $error =[];
-       $acc = $this->db->findUser($userName);
 
-       if(empty($userName))
-       {
-           $error = ['missing user name'];
-       }
-       elseif($acc != null)
-       {
-           if($acc->getUserName() == $userName)
-           {
-               $error = ['that user name is already in use'];
-           }
-       }
+    //check user name
+    private function checkUserName($userName)
+    {
+        $error = [];
+        $acc = $this->db->findUser($userName);
+
+        if (empty($userName)) {
+
+            $error = ['missing user name'];
+
+        } elseif ($acc != null) {
+
+            if ($acc->getUserName() == $userName) {
+
+                    $error = ['that user name is already in use'];
+            }
+        }
 
         return $error;
-   }
-    //hash password
-   private function hashPassword($password)
-   {
-       return password_hash($password, PASSWORD_DEFAULT);
-   }
+    }
 
+    //hash password
+    private function hashPassword($password)
+    {
+        return password_hash($password, PASSWORD_DEFAULT);
+    }
 }
